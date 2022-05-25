da35612b574d6d51a213ea986b92f6d4e3d6ee69
Rohan2596 <rohankadam662@gmail.com>
2022-05-25 09:01:25 +0530

    Solution:Move Zeros Using Java.

--- src/easy/MoveZeroes.java
+++ src/easy/MoveZeroes.java
@@ -1,18 +1,19 @@
 package easy;
 
+import java.util.Arrays;
+
 /**
  * *****************************
  * *******Problem Statement*****
  * *****************************
  *
- * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
- *
- * Note that you must do this in-place without making a copy of the array.
+ * Given an integer array nums, move all 0's to the end of it
+ * while maintaining the relative order of the non-zero elements.
+ *Note that you must do this in-place without making a copy of the array.
  *
  *
  *
  * Example 1:
- *
  * Input: nums = [0,1,0,3,12]
  * Output: [1,3,12,0,0]
  * Example 2:
@@ -33,6 +34,26 @@ package easy;
 
 public class MoveZeroes {
     public static void main(String[] args) {
+
+        int [] arr= new int[]{0,1,0,3,12};
+        int [] zeros= new int[arr.length];
+        int j=0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]==0){
+                zeros[(arr.length-1)]=arr[i];
+             //   j++;
+            }
+            if(arr[i]!=0){
+                zeros[j]=arr[i];
+                j++;
+            }
+        }
+
+        for (int i = 0; i < zeros.length; i++) {
+            System.out.println(zeros[i]);
+            arr[i]=zeros[i];
+        }
+
 
     }
 }
