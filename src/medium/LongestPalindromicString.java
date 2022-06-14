b0697ecd9e2414394f0e409fc0e2be703f47e776
Rohan2596 <rohankadam662@gmail.com>
2022-06-14 09:28:56 +0530

    Problem Statement: Longest Palindromic String from LeetCode.

---
+++ src/medium/LongestPalindromicString.java
@@ -0,0 +1,30 @@
+package medium;
+/***
+ * -------------------------------------------------
+ * ----------- Problem Statement -----------
+ * _________________________________________________
+ *
+ * Given a string s, return the longest palindromic substring in s.
+ *
+ * Example 1:
+ * Input: s = "babad"
+ * Output: "bab"
+ * Explanation: "aba" is also a valid answer.
+ *
+ * Example 2:
+ * Input: s = "cbbd"
+ * Output: "bb"
+ *
+ * Constraints:
+ * 1 <= s.length <= 1000
+ * s consist of only digits and English letters.
+ *
+ */
+
+
+
+public class LongestPalindromicString {
+    public static void main(String[] args) {
+
+    }
+}
