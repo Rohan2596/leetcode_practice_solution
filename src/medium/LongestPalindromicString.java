66b969472ea5c509eedfb4b1aca2f6a0bcec50cf
Rohan2596 <rohankadam662@gmail.com>
2022-06-15 10:01:07 +0530

    Solution:- Longest Palindromic String Using Java (Time Limit Exceed)

--- src/medium/LongestPalindromicString.java
+++ src/medium/LongestPalindromicString.java
@@ -25,6 +25,37 @@ package medium;
 
 public class LongestPalindromicString {
     public static void main(String[] args) {
-
+        String s = "ccc";
+        char[] chars = s.toCharArray();
+        int max=0;
+        String maxString="";
+        for (int i = 0; i < chars.length; i++) {
+            StringBuilder builder = new StringBuilder("");
+            builder.append(chars[i]);
+            for (int j = i+1; j < chars.length; j++) {
+                builder.append(chars[j]);
+                String currentString= builder.toString();
+                String reverseString=builder.reverse().toString();
+                if(currentString.equals(reverseString)){
+                    if(max<currentString.length()){
+                        max=currentString.length();
+                        maxString=currentString;
+                    }
+                }
+                builder=new StringBuilder(currentString);
+            }
+            if(max==0){
+                String currentString= builder.toString();
+                String reverseString=builder.reverse().toString();
+                if(currentString.equals(reverseString)){
+                    if(max<currentString.length()){
+                        max=currentString.length();
+                        maxString=currentString;
+                    }
+                }
+                builder=new StringBuilder(currentString);
+            }
+        }
+        System.out.println(max);
     }
 }
