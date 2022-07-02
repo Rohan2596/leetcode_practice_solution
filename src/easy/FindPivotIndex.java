4445c5cf11b427948e3377a365ab693d276ad177
Rohan2596 <rohankadam662@gmail.com>
2022-07-02 15:34:55 +0530

    Solution:Find Pivot Index from LeetCode using Java.

--- src/easy/FindPivotIndex.java
+++ src/easy/FindPivotIndex.java
@@ -4,20 +4,20 @@ package easy;
  * ------------------------------------
  * ------ Problem Statement----
  * ___________________________________
- *
- *
+ * <p>
+ * <p>
  * Given an array of integers nums, calculate the pivot index of this array.
- *
- * The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
- *
- * If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left. This also applies to the right edge of the array.
- *
+ * <p>
+ * The pivot index is the index where the sum of all the numbers strictly to
+ * the left of the index is equal to the sum of all the numbers strictly to the index's right.
+ * <p>
+ * If the index is on the left edge of the array, then the left sum is 0 because there
+ * are no elements to the left. This also applies to the right edge of the array.
+ * <p>
  * Return the leftmost pivot index. If no such index exists, return -1.
- *
- *
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: nums = [1,7,3,6,5,6]
  * Output: 3
  * Explanation:
@@ -25,28 +25,45 @@ package easy;
  * Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
  * Right sum = nums[4] + nums[5] = 5 + 6 = 11
  * Example 2:
- *
+ * <p>
  * Input: nums = [1,2,3]
  * Output: -1
  * Explanation:
  * There is no index that satisfies the conditions in the problem statement.
  * Example 3:
- *
+ * <p>
  * Input: nums = [2,1,-1]
  * Output: 0
  * Explanation:
  * The pivot index is 0.
  * Left sum = 0 (no elements to the left of index 0)
  * Right sum = nums[1] + nums[2] = 1 + -1 = 0
- *
- *
+ * <p>
+ * <p>
  * Constraints:
- *
+ * <p>
  * 1 <= nums.length <= 104
  * -1000 <= nums[i] <= 1000
- *
- * */
+ */
 
 
 public class FindPivotIndex {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3};
+        int lSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rSum = 0;
+            for (int j = nums.length - 1; j > i; j--) {
+                  rSum=rSum+nums[j];
+            }
+            if(rSum==lSum){
+                System.out.println(i);
+            }
+            lSum=lSum+nums[i];
+        }
+
+
+
+    }
 }
