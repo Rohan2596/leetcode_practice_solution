f136240e0f1d9fdd1ceeee7ab57d6df299b1a539
Rohan2596 <rohankadam662@gmail.com>
2022-07-11 18:49:38 +0530

    In-progress: Diagonal Traverse using java

--- src/medium/DiagonalTraverse.java
+++ src/medium/DiagonalTraverse.java
@@ -1,5 +1,7 @@
 package medium;
 
+import java.util.Arrays;
+
 /***
  *************************************
  * *** Problem Statement *********
@@ -30,5 +32,25 @@ public class DiagonalTraverse {
 
     public static void main(String[] args) {
 
+        int[][] mat={{1,2,3},{4,5,6},{7,8,9}};
+       // int[][] mat= {{1,2},{3,4}};
+        int m= mat.length;
+        int n=mat.length;
+        int [] trans= new int[m*n];
+        // 0*0  1*1 2*2
+        //  0+0  +4   4
+ int k=0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.println( "i:- "+ i +" j:- "+ j +" == " +mat[i][j]);
+                if(i==j){
+                    System.out.println((i*j)*4);
+                    int value= (i*j)*m;
+                    trans[2*((i+j)/m)]=mat[i][j];
+                }
+               // if()
+            }
+        }
+        Arrays.stream(trans).forEach(System.out::print);
     }
 }
