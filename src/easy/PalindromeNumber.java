f91759337cfc161fb690c8ef2d083153cd3a8739
Rohan2596 <rohankadam662@gmail.com>
2022-07-29 15:53:16 +0530

    Problem Statement:Palindrome Number From LeetCode.

---
+++ src/easy/PalindromeNumber.java
@@ -0,0 +1,38 @@
+package easy;
+
+/**
+ * Problem Statement
+ * Given an integer x, return true if x is palindrome integer.
+ * <p>
+ * An integer is a palindrome when it reads the same backward as forward.
+ * <p>
+ * For example, 121 is a palindrome while 123 is not.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: x = 121
+ * Output: true
+ * Explanation: 121 reads as 121 from left to right and from right to left.
+ * Example 2:
+ * <p>
+ * Input: x = -121
+ * Output: false
+ * Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+ * Example 3:
+ * <p>
+ * Input: x = 10
+ * Output: false
+ * Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * -231 <= x <= 231 - 1
+ */
+public class PalindromeNumber {
+
+    public static void main(String[] args) {
+
+    }
+}
